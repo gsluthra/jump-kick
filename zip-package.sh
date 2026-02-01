26b5849aee3e818f175756081f3dc6e30798c68d
Gurpreet Luthra <gsluthra@gmail.com>
2026-02-01 20:34:18 +0530

    Fixed manifest to ensure it passes Mozilla Developer Hub requirements. Also added zip file that was uploaded

---
+++ zip-package.sh
@@ -0,0 +1,9 @@
+zip -r jump-kick.zip \
+  manifest.json \
+  background.js \
+  popup.html popup.js popup.css \
+  options.html options.js options.css \
+  fuse.min.js \
+  icons \
+  LICENSE.md \
+  NOTICE.md
