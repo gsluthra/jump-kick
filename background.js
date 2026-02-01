db6a66eebaf1b75557ffc73844e25b6dd07e393b
Gurpreet Luthra <gsluthra@gmail.com>
2026-02-01 16:18:26 +0530

    Tooltip of extension shows the shortcut also

--- background.js
+++ background.js
@@ -3,3 +3,24 @@ browser.commands.onCommand.addListener((command) => {
     browser.browserAction.openPopup();
   }
 });
+
+async function updateTooltip() {
+  try {
+    const commands = await browser.commands.getAll();
+    const cmd = commands.find(c => c.name === "open-tab-switcher");
+
+    const shortcut = cmd && cmd.shortcut ? cmd.shortcut : "a shortcut";
+    const title = `foo-tab with [${shortcut}] to quickly search tabs`;
+
+    browser.browserAction.setTitle({ title });
+  } catch (e) {
+    console.error("Failed to set tooltip", e);
+  }
+}
+
+// Run on startup
+updateTooltip();
+
+// Also update if extension is reloaded or browser restarts
+browser.runtime.onInstalled.addListener(updateTooltip);
+browser.runtime.onStartup.addListener(updateTooltip);
