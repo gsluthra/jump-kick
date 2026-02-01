5cf0af3a56ace152f073a5e4ddafe1326f0b4af7
Gurpreet Luthra <gsluthra@gmail.com>
2026-02-01 15:38:40 +0530

    Added info in preferences to see the current shortcut.

--- options.js
+++ options.js
@@ -19,3 +19,18 @@ if (versionEl) {
   const manifest = browser.runtime.getManifest();
   versionEl.textContent = manifest.version;
 }
+
+//To see the current shortcut for opening the tab switcher
+async function loadShortcut() {
+  const commands = await browser.commands.getAll();
+  const cmd = commands.find(c => c.name === "open-tab-switcher");
+
+  const display = document.getElementById("shortcutDisplay");
+  if (!display) return;
+
+  display.textContent = cmd && cmd.shortcut
+    ? cmd.shortcut
+    : "Not set!";
+}
+
+loadShortcut();
